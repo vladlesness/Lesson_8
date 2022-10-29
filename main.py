babaea3d0034c0ef1384de12b38beafd6f7b66f7
Vlad Lesyk <vlad.lesyk@gmail.com>
2022-10-30 00:38:00 +0300

    Commit 1. Branch test. test_func

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def main_func():
     print('This is main_func. Commit 1')
     print('This is main_func. Commit 2')
     print('This is main_func. Commit 3')
+
+
+def test_func():
+    print('This is test_func. Branch test. Commit 1)
