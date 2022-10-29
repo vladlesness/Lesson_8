2e5b882c05cc115b163f288f5da9ee6212549155
Vlad Lesyk <vlad.lesyk@gmail.com>
2022-10-30 01:32:11 +0300

    Commit 1. Branch pycharmtest. py_charm_test_func

--- py_charm.py
+++ py_charm.py
@@ -1,3 +1,7 @@
 def py_charm_func():
     print('This is py_charm_func. Commit 7')
     print('This is py_charm_func. Commit 8')
+
+
+def py_charm_test_func():
+    print('This is py_charm_test_func. Branch pycharmtest. Commit 1')
