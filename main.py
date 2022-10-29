3222fa12efb2e57c30e1da61b19bb42868ec1966
Vlad Lesyk <vlad.lesyk@gmail.com>
2022-10-30 01:12:22 +0300

    Commit 6. fixed quote error

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def main_func2():
     print('This is main_func2. Commit 4')
 # ======= end of what was added in Commit 4, on branch main
 
+
 # this is what was added in Commit 2 on branch test
 def test_func():
-    print('This is test_func. Branch test. Commit 1)
-    print('This is test_func. Branch test. Commit 2)
+    print('This is test_func. Branch test. Commit 1')
+    print('This is test_func. Branch test. Commit 2')
 # >>>>>>> test --- end of what was added in Commit 2 on branch test
