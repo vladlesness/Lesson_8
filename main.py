d0914b83590268cad20c658dc963208aad4b31f3
Vlad Lesyk <vlad.lesyk@gmail.com>
2022-10-30 00:35:09 +0300

    Commit 4. main_func2

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def main_func():
     print('This is main_func. Commit 1')
     print('This is main_func. Commit 2')
     print('This is main_func. Commit 3')
+
+
+def main_func2():
+    print('This is main_func2. Commit 4')
